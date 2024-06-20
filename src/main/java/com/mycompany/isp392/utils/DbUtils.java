128e2280ae08ecb849472a3e1ab38f76883f6497
notlongfen <phannguyenhoanglong@gmail.com>
2024-06-20 19:26:14 +0700

    update

--- src/main/java/com/mycompany/isp392/utils/DbUtils.java
+++ src/main/java/com/mycompany/isp392/utils/DbUtils.java
@@ -8,6 +8,8 @@ public class DbUtils {
     private static final String DB_NAME = "ecomerce";
     private static final String DB_USERNAME = "sa";
     private static final String DB_PASS = "Abc123@$";
+    private static final String moreva = "abcxyz";
+    
 
     public static Connection getConnection() throws ClassNotFoundException, SQLException {
         Connection conn = null;
